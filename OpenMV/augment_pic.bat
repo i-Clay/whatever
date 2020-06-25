cfc3b0501be469fe612867309b75d0f48a26cffb
杨家辉 <54164071+i-Clay@users.noreply.github.com>
2020-06-25 21:25:37 +0800

    Create augment_pic.bat

---
+++ OpenMV/augment_pic.bat
@@ -0,0 +1,20 @@
+python augment_images.py --input MY_numbers/ZERO/ --output MY_numbers/0_ZERO/ --count 20
+echo.
+python augment_images.py --input MY_numbers/ONE/ --output MY_numbers/1_ONE/ --count 20
+echo.
+python augment_images.py --input MY_numbers/TWO/ --output MY_numbers/2_TWO/ --count 20
+echo.
+python augment_images.py --input MY_numbers/THREE/ --output MY_numbers/3_THREE/ --count 20
+echo.
+python augment_images.py --input MY_numbers/FOUR/ --output MY_numbers/4_FOUR/ --count 20
+echo.
+python augment_images.py --input MY_numbers/FIVE/ --output MY_numbers/5_FIVE/ --count 20
+echo.
+python augment_images.py --input MY_numbers/SIX/ --output MY_numbers/6_SIX/ --count 20
+echo.
+python augment_images.py --input MY_numbers/SEVEN/ --output MY_numbers/7_SEVEN/ --count 20
+echo.
+python augment_images.py --input MY_numbers/EIGHT/ --output MY_numbers/8_EIGHT/ --count 20
+echo.
+python augment_images.py --input MY_numbers/NINE/ --output MY_numbers/9_NINE/ --count 20
+pause
